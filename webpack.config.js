97f3f34172c3d1a2ec234dfce3064e35947fddbd
Eric Schaefer <omg@eric-schaefer.com>
2015-07-22 17:15:13 +0200

    change webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,11 @@ module.exports = {
   output: {
     filename: 'bundle.min.js'
   },
+  devServer: {
+    noInfo: true,
+    inline: true,
+    port: 3000
+  },
   module: {
     loaders: [
       { test: /\.jsx$/, exclude: /amaphiko-web\/node_modules/, loaders: jsxLoaders },
